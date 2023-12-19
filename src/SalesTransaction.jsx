829a3d93b3e5b3662de4e3aa23e63adb19fff23b
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-12-19 23:43:17 +0530

    Update SalesTransaction.jsx

--- src/SalesTransaction.jsx
+++ src/SalesTransaction.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSalesSuccess } from "./actions/actioncreators";
+import { fetchSalesSuccess, fetchItemsSuccess} from "./actions/actioncreators";
 import SalesReportItem from "./Components/SalesReportItem";
 
 export default function SalesTransaction(){
@@ -13,11 +13,28 @@ export default function SalesTransaction(){
 
 
 
+    const inventoryList = useSelector((state) => state.itemsList);
+
+
+    useEffect(()=>{dispatch(fetchItemsSuccess())},[])
+
     useEffect(()=>{dispatch(fetchSalesSuccess())},[])
+
+
+    function calculateTotalCost(){
+        return inventoryList.reduce((acc, currentItem)=>(acc+ currentItem.price*currentItem.quantity),0)
+    }
+
+    function calculateTotalSales(){
+        return salesList.reduce((acc, currentItem)=>(acc+currentItem.amount*currentItem.quantity),0)
+    }
 
 
     return(
         <div>
+
+         <h1> Total Cost: {calculateTotalCost()} </h1>
+         <h1> Total Sales: {calculateTotalSales()} </h1>
          { salesList.map((sale)=>(
             <SalesReportItem item = {sale} key={sale._id}/>
          ))}
